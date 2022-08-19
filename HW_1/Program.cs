64641894f2a324a30026b60a488b3fbdae409249
Yan <yangalperin@gmail.com>
2022-08-19 06:57:54 +0300

    HW1

---
+++ HW_1/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+int a = ReadInt("Введите первое число: ");
+int b = ReadInt("Введите второе число: ");
+
+int multyply = 1;
+for (int i = 1; i <= b; i++)
+{
+    multyply = multyply * a;
+}
+Console.WriteLine("число " + a + " в степени " + b + " составляет " + multyply);
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+
